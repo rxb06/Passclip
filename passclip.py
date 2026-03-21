e86bb36ce6858147e6e609b29af4758db09b2e34
Rutvik Belapurkar <rutvik.belapurkar@quodorbis.com>
2026-03-21 02:41:02 +0000

    Bump version to 1.1.1

--- passclip.py
+++ passclip.py
@@ -28,7 +28,7 @@ Shell shortcuts (inside the interactive shell):
   o gmail                           # Copy OTP code (fuzzy)
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 import argparse
 import cmd
